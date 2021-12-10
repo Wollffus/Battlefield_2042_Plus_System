6b6b3f7299a865dbc817213fcee0b309d2362eef
Wollffus <58875783+Wollffus@users.noreply.github.com>
2021-12-09 18:50:41 -0800

    update: version

--- Battlefield_2042_Plus_System/fxmanifest.lua
+++ Battlefield_2042_Plus_System/fxmanifest.lua
@@ -2,13 +2,13 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'https://discord.gg/zGVDppYxfk'
-version '2.1.5'
+version '1.1.5'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-ui_page 'HUD/index.html'
-
 lua54 'yes'
+
+ui_page 'HUD/index.html'
 
 server_script {
     'ensure.lua'
@@ -23,53 +23,5 @@ files {
     'HUD/Cross.png',
     'HUD/style.css',
     'HUD/script.js',
-    'HUD/images/*',
-    'HUD/images/Advanced_Rifle/*',
-    'HUD/images/ap_pistol/*',
-    'HUD/images/Assault_Rifle/*',
-    'HUD/images/Assault_Rifle_Mk2/*',
-    'HUD/images/Assault_Shotgun/*',
-    'HUD/images/assault_smg/*',
-    'HUD/images/Bullpup_Rifle/*',
-    'HUD/images/Bullpup_Rifle_Mk2/*',
-    'HUD/images/Bullpup_Shotgun/*',
-    'HUD/images/Carbine_Rifle/*',
-    'HUD/images/Carbine_Rifle_Mk2/*',
-    'HUD/images/Ceramic_Pistol/*',
-    'HUD/images/Combat _PDW/*',
-    'HUD/images/Combat_MG/*',
-    'HUD/images/Combat_MG_Mk2/*',
-    'HUD/images/combat_pistol/*',
-    'HUD/images/Combat_Shotgun/*',
-    'HUD/images/Compact_Rifle/*',
-    'HUD/images/Grenade_Launcher/*',
-    'HUD/images/Gusenberg_Sweeper/*',
-    'HUD/images/heavy_pistol/*',
-    'HUD/images/heavy_r_mk_2/*',
-    'HUD/images/Heavy_Shotgun/*',
-    'HUD/images/Heavy_Sniper/*',
-    'HUD/images/Heavy_Sniper_Mk2/*',
-    'HUD/images/m_pistol/*',
-    'HUD/images/Marksman_Rifle/*',
-    'HUD/images/Marksman_Rifle_Mk2/*',
-    'HUD/images/MG/*',
-    'HUD/images/Micro_SMG/*',
-    'HUD/images/Military_Rifle/*',
-    'HUD/images/Mini_SMG/*',
-    'HUD/images/pistol/*',
-    'HUD/images/pistol_50/*',
-    'HUD/images/pistol_mk2/*',
-    'HUD/images/Pump_Shotgun/*',
-    'HUD/images/Pump_Shotgun_Mk2/*',
-    'HUD/images/smg/*',
-    'HUD/images/SMG_Mk2/*',
-    'HUD/images/Sniper_Rifle/*',
-    'HUD/images/SNS_Pistol/*',
-    'HUD/images/Special_Carbine/*',
-    'HUD/images/Sig_P320/*',
-    'HUD/images/Glock/*',
-    'HUD/images/MP5/*',
-    'HUD/images/M9A3/*',
-    'HUD/images/m700/*',
-    'HUD/images/KnightsArmament_SR25/*'
+    'HUD/images/*'
 }
